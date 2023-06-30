71de0813807041798544f973a87b4fa3118b400f
Aldanov <aldanov@ukr.net>
2023-07-01 00:48:07 +0300

    example

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/example/:word', (req, res) => {
+  res.send('example:'+req.params.word)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
